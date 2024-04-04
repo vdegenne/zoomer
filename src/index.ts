9c2de4a1278dff8812f9d54a3a5decc4ebd304bc
vdegenne <vdegenne@gmail.com>
2024-04-04 18:29:43 +0200

    2024-04-04

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,12 @@ declare global {
 }
 
 const url = 'https://shop.r10s.jp/book/cabinet/1432/9784581021432.jpg';
-// console.log(url);
-//
-// const img = new Image();
-// img.src = url;
-// document.body.appendChild(img);
-const osd = openseadragon({
-	element: window.openseadragonContainer,
-	tileSources: url,
-});
+console.log(url);
+
+const img = new Image();
+img.src = url;
+document.body.appendChild(img);
+// const osd = openseadragon({
+// 	element: window.openseadragonContainer,
+// 	tileSources: url,
+// });
